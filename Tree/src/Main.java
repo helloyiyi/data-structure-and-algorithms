cd82f0490c327966a86d5af40b7fd12a39ce8cb1
Yiyi <daiyiyi1991@gmail.com>
2018-06-28 22:56:25 -0400

    BST for Person with different length of name(support the same length -> left child)

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -1,10 +1,10 @@
 public class Main{
     public static void main(String[] args){
-        Person person1 = new Person("yiyi", "18");
-        Person person2 = new Person("xiaoxi", "20");
-        Person person3 = new Person("andy", "30");
-        Person person4 = new Person("rachel", "25");
-        Person person5 = new Person("sree", "19");
+        Person person1 = new Person("22", "18");
+        Person person2 = new Person("1", "20");
+        Person person3 = new Person("333", "30");
+        Person person4 = new Person("444", "25");
+        Person person5 = new Person("555", "19");
 
         BinarySearchTree bst = new BinarySearchTree();
         bst.insert(person1);
@@ -12,9 +12,13 @@ public class Main{
         bst.insert(person3);
         bst.insert(person4);
         bst.insert(person5);
-        bst.showAll(bst.findNode("yiyi"));
+        bst.showAll(bst.findNode("22"));
 
-        System.out.print(bst.getData( bst.findParent("rachel")));
+        //System.out.println(bst.getData( bst.findParent("1")));
+        //System.out.println(bst.getData( bst.findParent("22")));
+        // System.out.println(bst.getData( bst.findParent("333")));
+        System.out.println(bst.getData( bst.findParent("333")));
+        // System.out.println(bst.getData( bst.findParent("55555")));
        
 
 
